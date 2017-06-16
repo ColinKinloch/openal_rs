e0d750e93f5d969c42b0f7cc00128d44e757c8c7
Colin Kinloch <colin@kinlo.ch>
2017-06-16 13:34:46 +0100

    updated bindgen

--- gen.sh
+++ gen.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
-bindgen ../openal-soft/include/AL/al.h --link=dynamic=openal --convert-macros --macro-int-types=sint,sint,sint,sint,sint,sint,sint,sint --output src/al.rs -- -I /opt/android-ndk-r13b/platforms/android-19/arch-arm/usr/include -D__ANDROID__
-bindgen ../openal-soft/include/AL/alc.h --link=dynamic=openal --convert-macros --macro-int-types=sint,sint,sint,sint,sint,sint,sint,sint --output src/alc.rs -- -I /opt/android-ndk-r13b/platforms/android-19/arch-arm/usr/include -D__ANDROID__
-bindgen ../openal-soft/include/AL/alext.h --link=dynamic=openal --convert-macros --macro-int-types=sint,sint,sint,sint,sint,sint,sint,sint --builtins --output src/alext.rs -- -I /opt/android-ndk-r13b/platforms/android-19/arch-arm/usr/include -D__ANDROID__
+ANDROID_INCLUDE="${NDK_HOME}/platforms/android-21/arch-arm/usr/include"
+bindgen ../openal-soft/include/AL/al.h -lopenal --no-unstable-rust --output src/al.rs -- -I ${ANDROID_INCLUDE} -D__ANDROID__
+bindgen ../openal-soft/include/AL/alc.h -lopenal --no-unstable-rust --output src/alc.rs -- -I ${ANDROID_INCLUDE} -D__ANDROID__
+bindgen ../openal-soft/include/AL/alext.h -lopenal --no-unstable-rust --builtins --output src/alext.rs -- -I ${ANDROID_INCLUDE} -D__ANDROID__
